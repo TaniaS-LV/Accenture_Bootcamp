ac6a5d5d15a0c6ac8bbfdb3eb3d2096a21f38662
Tatjana.Sporihina <t.sporihina@gmail.com>
2023-03-16 18:44:22 +0200

    Added tasks 16/03/2023

---
+++ Tasks/strings_lists.py
@@ -0,0 +1,38 @@
+# ------TASK 1 -------
+text = input('Enter a text: ').lower()
+letter = input('Enter a letter: ').lower()
+answer = text.count(letter)
+print(f'Letter "{letter}" has {answer} occurrences in the text.\n')
+
+# ------TASK 1.1 -------
+print('\nOccurences in the text:')
+text_unique = []
+text_unique_count = []
+for t in text:
+    if t.isalpha() and t not in text_unique:
+        text_unique.append(t)
+for t in text_unique:
+    print(f'{t}: {text.count(t)} times.')
+    text_unique_count.append(text.count(t))
+
+# ------TASK 2 -------
+#Sorting without using sort()
+text_sorted = [None] *26
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for t in text_unique:
+    i = alphabet.index(t)
+    text_sorted[i] = t
+text_sorted = [e for e in text_sorted if e is not None]
+
+#Print occurences in alhpabet order
+print('\nOccurences in alhpabetical order:')
+for x in text_sorted:
+    i = text_unique.index(x)
+    print(f'{x}: {text_unique_count[i]}')
+
+#Print occurences in numerical order
+print('\nOccurences in descending order:')
+pairs = zip(text_unique, text_unique_count)
+sorted_pairs = sorted(pairs, key=lambda x: x[1], reverse=True)
+for pair in sorted_pairs:
+    print(f"{pair[0]}: {pair[1]}")
