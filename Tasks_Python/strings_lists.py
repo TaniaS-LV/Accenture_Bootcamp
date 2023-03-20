e3234503d56fc759dcd95e37b2a00724ddb4f45e
Tatjana.Sporihina <t.sporihina@gmail.com>
2023-03-20 10:22:18 +0200

    test'

--- Tasks_Python/strings_lists.py
+++ Tasks_Python/strings_lists.py
@@ -16,21 +16,41 @@ for t in text_unique:
     text_unique_count.append(text.count(t))
 
 # ------TASK 2 -------
-#Sorting without using sort()
-text_sorted = [None] *26
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# Sorting without using sort()
+text_sorted = [None] * 26
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
 for t in text_unique:
     i = alphabet.index(t)
     text_sorted[i] = t
 text_sorted = [e for e in text_sorted if e is not None]
 
-#Print occurences in alhpabet order
+# Alternative solution 1
+text_sorted = []
+text_unique_copy = text_unique.copy()
+while text_unique_copy:
+    min_value = text_unique_copy[0]
+    for t in text_unique_copy:
+        if t < min_value:
+            min_value = t
+    text_sorted.append(min_value)
+    text_unique_copy.remove(min_value)
+
+# Alternative solution 2
+text_sorted = []
+text_unique_copy = text_unique.copy()
+while text_unique_copy:
+    min_value = min(text_unique_copy)
+    text_sorted.append(min_value)
+    text_unique_copy.remove(min_value)
+
+# Print occurences in alhpabet order
 print('\nOccurences in alhpabetical order:')
 for x in text_sorted:
     i = text_unique.index(x)
     print(f'{x}: {text_unique_count[i]}')
 
-#Print occurences in numerical order
+# #Print occurences in numerical order
 print('\nOccurences in descending order:')
 pairs = zip(text_unique, text_unique_count)
 sorted_pairs = sorted(pairs, key=lambda x: x[1], reverse=True)
